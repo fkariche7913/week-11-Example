ba465feaa405ee1eb3b4ca3ad53d72da3b42654d
fkariche7913 <130328045+fkariche7913@users.noreply.github.com>
2023-04-11 14:58:57 -0400

    Add new Java program
    print"Hello" and 1 through 100 with for loop.

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test
+{
+   public static void main(String[] args)
+   {
+      System.out.println("HELLO WORD");
+      int i =1;
+      for(i=1; i<=100 ;i++)
+      {
+         System.out.print(i +" ");
+      }
+   } 
+}
